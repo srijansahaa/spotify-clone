2554f3f016750e8bd9c1f97bab704fe6f2501730
Aditipatel02 <56363123+Aditipatel02@users.noreply.github.com>
2021-08-02 16:59:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./App.css";
+
 function App() {
-    return <div className="app">React App</div>;
+    return <div className="app">
+        
+        React App
+        
+        </div>;
 }
 export default App;

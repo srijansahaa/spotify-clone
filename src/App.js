99bc4495f61b7b6a3c33b03d8d51007e5f45c21b
Srijan Saha <srijan1336@gmail.com>
2021-08-02 17:58:55 +0530

    added login and spotify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-    return <div className="app">
-        
-        React App
-        
-        </div>;
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
+  return (
+    <div className="app">
+      {token ? 
+      <h1>Logged in</h1> 
+      : 
+      <Login />}
+    </div>
+  )
 }
+
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Login.css";
+import { loginUrl } from "./spotify";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
+        alt="Spotify logo"
+      />
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
